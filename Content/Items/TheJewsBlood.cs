84062794492ea40ddfecddbed17a0de75bd0ff12
Svenkia <contrinostefano4@gmail.com>
2026-07-14 16:57:17 +0200

    Modified the code

--- Content/Items/TheJewsBlood.cs
+++ Content/Items/TheJewsBlood.cs
@@ -1,55 +1,37 @@
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-using Terraria.DataStructures;
-using Microsoft.Xna.Framework;
 
 namespace TheLawOfTheGods.Content.Items
 {
+	// This is a basic item template.
+	// Please see tModLoader's ExampleMod for every other example:
+	// https://github.com/tModLoader/tModLoader/tree/stable/ExampleMod
 	public class TheJewsBlood : ModItem
 	{
+		// The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.TutorialMod.hjson' file.
 		public override void SetDefaults()
 		{
 			Item.damage = 10000;
 			Item.DamageType = DamageClass.Melee;
-
-			Item.width = 32;
-			Item.height = 32;
-
-			Item.useTime = 40;
+			Item.width = 40;
+			Item.height = 40;
+			Item.useTime = 10;
 			Item.useAnimation = 40;
 			Item.useStyle = ItemUseStyleID.Swing;
-
 			Item.knockBack = 6;
-
+			Item.value = Item.buyPrice(silver: 1);
+			Item.rare = ItemRarityID.Blue;
 			Item.UseSound = SoundID.Item1;
 			Item.autoReuse = true;
-
-			Item.shoot = ModContent.ProjectileType<Projectiles.TheJewsBloodSlash>();
-			Item.noUseGraphic = true;
 		}
 
-
-		public override bool Shoot(
-			Player player,
-			EntitySource_ItemUse_WithAmmo source,
-			Vector2 position,
-			Vector2 velocity,
-			int type,
-			int damage,
-			float knockBack)
+		public override void AddRecipes()
 		{
-			Projectile.NewProjectile(
-				source,
-				player.Center,
-				velocity,
-				type,
-				damage,
-				knockBack,
-				player.whoAmI
-			);
-
-			return false;
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.DirtBlock, 10);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.Register();
 		}
 	}
 }
