602cbfe964573bb237e76cd33463d85d02092db9
Svenkia <contrinostefano4@gmail.com>
2026-07-16 17:45:37 +0200

    Removed the test NPC and added a new one called Charles. Added his png and code files

--- NPCs/TownNPCs/Charles.cs
+++ NPCs/TownNPCs/Charles.cs
@@ -54,11 +54,40 @@ namespace TheLawOfTheGods.NPCs.TownNPCs
             NPC.DeathSound = SoundID.NPCDeath1;
             NPC.knockBackResist = 0.5f;
             AnimationType = NPCID.PartyGirl;
+            NPC.Happiness
+            .SetBiomeAffection<OceanBiome>(AffectionLevel.Love);
         }
 
         public override string GetChat()
         {
-            return "Ciao, sono un NPC di prova.";
+            return "Hi folk. I don't really like people, but you... you seem like someone who can understand me.";
+        }
+
+        public override void SetChatButtons(ref string button, ref string button2)
+        {
+            button = "Dialogue";
+            button2 = null;
+        }
+
+        public override void OnChatButtonClicked(bool firstButton, ref string shopName)
+        {
+            if (firstButton)
+            {
+                WeightedRandom<string> dialogue = new WeightedRandom<string>();
+
+                dialogue.Add("The sea remembers everything. Every ship that sinks, every name that is forgotten...");
+                dialogue.Add("Do not listen too closely to the waves at night. Sometimes they whisper things that were never meant for human ears.");
+                dialogue.Add("My ancestors made a pact beneath the cold waters. They gained eternity... but lost something far more precious.");
+                dialogue.Add("There are ruins beneath the ocean older than any kingdom of man. Some things down there still wait for their return.");
+                dialogue.Add("The people of this town used to pray to the stars. Now they pray to what lies beyond the waves.");
+                dialogue.Add("Have you ever wondered why the fishermen never speak about what they find in the deep?");
+                dialogue.Add("The fog around this place is not natural. It hides what should remain unseen.");
+                dialogue.Add("I once saw a creature rising from the abyss. I still dream of its impossible shape.");
+                dialogue.Add("The old families of Innsmouth knew the truth: the ocean was never empty.");
+                dialogue.Add("There are voices beneath the tides calling my name. I fear that one day I will answer.");
+
+                Main.npcChatText = dialogue;
+            }
         }
 
 
@@ -81,9 +110,6 @@ namespace TheLawOfTheGods.NPCs.TownNPCs
                 }
             }
         }
-
-        // Make this Town NPC teleport to the Queen or King statue when triggered.
-        public override bool CanGoToStatue(bool toKingStatue) => true;
 
     }
 }
