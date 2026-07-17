f071d8feb87a94281e54cd65fea950353a02614e
Svenkia <contrinostefano4@gmail.com>
2026-07-17 13:54:01 +0200

    Update Charles.cs

--- NPCs/TownNPCs/Charles.cs
+++ NPCs/TownNPCs/Charles.cs
@@ -44,8 +44,8 @@ namespace TheLawOfTheGods.NPCs.TownNPCs
             NPC.townNPC = true;
             NPC.friendly = true;
             NPC.lavaImmune = false;
-            NPC.width = 32;
-            NPC.height = 50;
+            NPC.width = 20;
+            NPC.height = 38;
             NPC.aiStyle = NPCAIStyleID.Passive;
             NPC.damage = 10;
             NPC.defense = 15;
